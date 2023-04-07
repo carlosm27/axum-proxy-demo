d922080fa12b6348a1fb22a325c775090e52e72f
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-04-07 13:11:07 -0400

    Add `check_address_block`

--- src/helpers.rs
+++ src/helpers.rs
@@ -9,3 +9,16 @@ pub fn read_file_lines_to_vec(filename: &str) -> io::Result<Vec<String>> {
     let file_reader = BufReader::new(file_in); 
     Ok(file_reader.lines().filter_map(io::Result::ok).collect()) 
 } 
+
+pub fn check_address_block(address_to_check: &str) -> bool {
+    let addresses_blocked = read_file_lines_to_vec(&"./blacklist.txt".to_string());
+    let addresses_blocked_iter: Vec<String> = match addresses_blocked {
+        Ok(vector) => vector,
+        Err(_) => vec!["Error".to_string()]
+    };
+    
+    let address_in = addresses_blocked_iter.contains(&address_to_check.to_string());
+    return address_in
+    
+
+}
